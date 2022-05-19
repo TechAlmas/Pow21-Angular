fc4224362959d704a3a52d48998c6541605d5752
Mrunal13 <mrunal.vora13@gmail.com>
2022-05-19 17:34:43 +0530

    additional changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  //apiUrl: 'https://devapp.pow21.com/api/',
+  apiUrl: 'https://www.app.pow21.com/api/',
+  apiKey: '57289d1b6580bc64a58830e09031e745'
+};

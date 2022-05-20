4bfc39340fe51a64aec948f4340767175f41407b
Mrunal13 <mrunal.vora13@gmail.com>
2022-05-20 09:00:05 +0530

    push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,15 @@
+<<<<<<< Updated upstream
 export const environment = {
   production: true,
   //apiUrl: 'https://devapp.pow21.com/api/',
   apiUrl: 'https://www.app.pow21.com/api/',
   apiKey: '57289d1b6580bc64a58830e09031e745'
 };
+=======
+export const environment = {
+  production: true,
+  apiUrl: 'https://app.pow21.com/api/',
+  //apiUrl: 'http://localhost/pow21-laravel/public/api/',
+  apiKey: '57289d1b6580bc64a58830e09031e745'
+};
+>>>>>>> Stashed changes

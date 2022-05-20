71e6f32453e45fa495aea8b2f58f9c983b8b008d
Mrunal13 <mrunal.vora13@gmail.com>
2022-05-20 08:50:55 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost/pow21-laravel/public/api/',
+  apiUrl: 'https://app.pow21.com/api/',
+  //apiUrl: 'http://localhost/pow21-laravel/public/api/',
   apiKey: '57289d1b6580bc64a58830e09031e745'
 };
 

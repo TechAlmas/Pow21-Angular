59fb5fbd560e57d2d3c4aab31e5509d7c375456a
Mrunal13 <mrunal.vora13@gmail.com>
2022-05-24 13:25:57 +0530

    push the files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -8,31 +8,3 @@ export const environment = {
   apiUrl: 'https://www.app.pow21.com/api/',
   apiKey: '57289d1b6580bc64a58830e09031e745'
 };
-
-/*
- * In development mode, for easier debugging, you can ignore zone related error
- * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
- * because it will have a performance impact when errors are thrown
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-=======
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: true,
-  apiUrl: 'https://app.pow21.com/api/',
-  //apiUrl: 'http://localhost/pow21-laravel/public/api/',
-  apiKey: '57289d1b6580bc64a58830e09031e745'
-};
-
-/*
- * In development mode, for easier debugging, you can ignore zone related error
- * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
- * because it will have a performance impact when errors are thrown
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
->>>>>>> Stashed changes

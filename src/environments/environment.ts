299910d5988ddd74d55df479f99cc47fc2bf891d
jayant-TA <105401876+jayant-TA@users.noreply.github.com>
2022-05-31 16:32:59 +0530

    Dispensory reviews fixes

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost/pow21-laravel/public/api/',
+  apiKey: '57289d1b6580bc64a58830e09031e745'
+};

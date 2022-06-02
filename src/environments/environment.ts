b21166b529ac2977f971af3ae3ef2b3fcfb19ba3
Mrunal13 <mrunal.vora13@gmail.com>
2022-06-02 18:05:26 +0530

    Enviournment again

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,10 @@
 
 export const environment = {
   production: true,
+<<<<<<< Updated upstream
   apiUrl: 'https://www.app.pow21.com/api/',
+=======
+  apiUrl: 'https://devapp.pow21.com/api/',
+>>>>>>> Stashed changes
   apiKey: '57289d1b6580bc64a58830e09031e745'
 };
